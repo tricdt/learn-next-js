afe180f2c823679eff3bd1446bcf0fb3b55fbe48
Nguyễn Trần Hồng Trí <tricdt@gmail.com>
2022-08-19 22:44:18 +0700

    Detail product

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,10 @@ class MyDocument extends Document {
                   href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.4.0/mdb.min.css"
                   rel="stylesheet"
                />
+               <link
+                  href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+                  rel="stylesheet"
+               />
                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
                <script src="https://kit.fontawesome.com/a076d05399.js"></script>
                <script
