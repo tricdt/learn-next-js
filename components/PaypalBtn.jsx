d170f36b67cdcb05d0112b5fbc5cacac096175f5
Nguyễn Trần Hồng Trí <tricdt@gmail.com>
2022-08-23 14:51:19 +0700

    Order payment

--- components/PaypalBtn.jsx
+++ components/PaypalBtn.jsx
@@ -1,56 +1,79 @@
-import React, { useEffect, useRef } from 'react'
-import { postData } from '../utils/fetchData'
+import React, { useContext, useEffect, useRef } from 'react'
+import { updateItem } from '../store/Actions'
+import { DataContext } from '../store/GlobalState'
+import { patchData } from '../utils/fetchData'
 
-const paypalBtn = ({ total, address, mobile, state, dispatch }) => {
+const paypalBtn = ({ order }) => {
    const refPaypalBtn = useRef()
-   const { auth, cart } = state
+   const { state, dispatch } = useContext(DataContext)
+   const { auth, orders } = state
    useEffect(() => {
       paypal
          .Buttons({
             style: {
-               shape: 'rect',
-               color: 'gold',
+               shape: 'pill',
+               color: 'blue',
                layout: 'vertical',
-               label: 'paypal',
+               label: 'buynow',
             },
             // Order is created on the server and the order id is returned
             createOrder: function (data, actions) {
                return actions.order.create({
                   purchase_units: [
                      {
-                        description: 'Thanh toán hóa đơn',
-                        amount: { currency_code: 'USD', value: total },
+                        amount: { currency_code: 'USD', value: order.total },
                      },
                   ],
                })
             },
-            // Finalize the transaction on the server after payer approval
-            // onApprove: (data, actions) => {
-            //    return actions.order.capture().then(function (orderData) {
-            //       postData('order', { address, mobile, cart, total }, auth.token).then((res) => console.log({ res }))
-            //    })
-            // },
+
             onApprove: function (data, actions) {
-               return actions.order.capture().then(function (orderData) {
+               return actions.order.capture().then(function (details) {
+                  console.log('Paypal')
                   dispatch({ type: 'NOTIFY', payload: { loading: true } })
-                  postData('order', { address, mobile, cart, total }, auth.token).then((res) => {
-                     if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: err.message } })
-                     dispatch({ type: 'ADD_CART', payload: [] })
+                  patchData(
+                     `order/payment/${order._id}`,
+                     {
+                        paymentId: details.payer.payer_id,
+                     },
+                     auth.token
+                  ).then((res) => {
+                     if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err } })
+
+                     dispatch(
+                        updateItem(
+                           orders,
+                           order._id,
+                           {
+                              ...order,
+                              paid: true,
+                              dateOfPayment: details.create_time,
+                              paymentId: details.payer.payer_id,
+                              method: 'Paypal',
+                           },
+                           'ADD_ORDERS'
+                        )
+                     )
+
                      return dispatch({ type: 'NOTIFY', payload: { success: res.msg } })
                   })
-
+                  // postData('order', { address, mobile, cart, total }, auth.token).then((res) => {
+                  //    if (res.err)
+                  //       return dispatch({ type: 'NOTIFY', payload: { error: err.message } })
+                  //    dispatch({ type: 'ADD_CART', payload: [] })
+                  //    return dispatch({ type: 'NOTIFY', payload: { success: res.msg } })
+                  // })
                   // Show a success message within this page, e.g.
                   // const element = document.getElementById('paypal-button-container')
                   // element.innerHTML = ''
                   // element.innerHTML = '<h3>Thank you for your payment!</h3>'
-
                   // Or go to another URL:  actions.redirect('thank_you.html');
                })
             },
          })
          .render(refPaypalBtn.current)
    }, [])
-   return <div ref={refPaypalBtn}></div>
+   return <div id="paypal-button-container" ref={refPaypalBtn}></div>
 }
 
 export default paypalBtn
@@ -66,16 +89,16 @@ export default paypalBtn
     function initPayPalButton() {
       paypal.Buttons({
         style: {
-          shape: 'rect',
-          color: 'gold',
+          shape: 'pill',
+          color: 'blue',
           layout: 'vertical',
-          label: 'paypal',
+          label: 'buynow',
           
         },
 
         createOrder: function(data, actions) {
           return actions.order.create({
-            purchase_units: [{"amount":{"currency_code":"USD","value":2}}]
+            purchase_units: [{"amount":{"currency_code":"USD","value":1}}]
           });
         },
 
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,21 +1,21 @@
-import React, { useContext, useEffect, useState } from 'react'
 import Head from 'next/head'
-import { DataContext } from '../store/GlobalState'
-import CartItem from '../components/CartItem'
 import Link from 'next/link'
-import { getData } from '../utils/fetchData'
-import PaypalBtn from '../components/paypalBtn'
+import React, { useContext, useEffect, useState } from 'react'
+import CartItem from '../components/CartItem'
+import { DataContext } from '../store/GlobalState'
+import { getData, postData } from '../utils/fetchData'
+import { useRouter } from 'next/router'
 const Cart = () => {
    const { state, dispatch } = useContext(DataContext)
    const { cart, auth, orders } = state
 
    const [address, setAddress] = useState('')
    const [mobile, setMobile] = useState('')
-   const [payment, setPayment] = useState(false)
 
    const [total, setTotal] = useState(0)
 
    const [callback, setCallback] = useState(false)
+   const router = useRouter()
 
    useEffect(() => {
       const getTotal = () => {
@@ -56,12 +56,45 @@ const Cart = () => {
          updateCart()
       }
    }, [callback])
-   const handlePayment = () => {
+   const handlePayment = async () => {
       if (!address || !mobile)
-         return dispatch({ type: 'NOTIFY', payload: { error: 'Please add your address and mobile.' } })
-      setPayment(true)
+         return dispatch({
+            type: 'NOTIFY',
+            payload: { error: 'Please add your address and mobile.' },
+         })
+      let newCart = []
+      for (const item of cart) {
+         const res = await getData(`product/${item._id}`)
+         if (res.product.inStock - item.quantity >= 0) {
+            newCart.push(item)
+         }
+      }
+      if (newCart.length < cart.length) {
+         setCallback(!callback)
+         return dispatch({
+            type: 'NOTIFY',
+            payload: {
+               error: 'The product is out of stock or the quantity is insufficient.',
+            },
+         })
+      }
+      dispatch({ type: 'NOTIFY', payload: { loading: true } })
+      postData('order', { address, mobile, cart, total }, auth.token).then((res) => {
+         if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err } })
+
+         dispatch({ type: 'ADD_CART', payload: [] })
+
+         const newOrder = {
+            ...res.newOrder,
+            user: auth.user,
+         }
+         dispatch({ type: 'ADD_ORDERS', payload: [...orders, newOrder] })
+         dispatch({ type: 'NOTIFY', payload: { success: res.msg } })
+         return router.push(`/order/${res.newOrder._id}`)
+      })
    }
-   if (cart.length === 0) return <img className="img-responsive w-100" src="/empty_cart.jpg" alt="not empty" />
+   if (cart.length === 0)
+      return <img className="img-responsive w-100" src="/empty_cart.jpg" alt="not empty" />
    return (
       <div className="row mx-auto">
          <Head>
@@ -107,15 +140,11 @@ const Cart = () => {
             <h3>
                Total: <span className="text-danger">${total}</span>
             </h3>
-            {payment ? (
-               <PaypalBtn total={total} address={address} mobile={mobile} state={state} dispatch={dispatch} />
-            ) : (
-               <Link href={auth.user ? '#!' : '/signin'}>
-                  <a className="btn btn-dark my-2" onClick={handlePayment}>
-                     Proceed with payment
-                  </a>
-               </Link>
-            )}
+            <Link href={auth.user ? '#!' : '/signin'}>
+               <a className="btn btn-dark my-2" onClick={handlePayment}>
+                  Proceed with payment
+               </a>
+            </Link>
          </div>
       </div>
    )
